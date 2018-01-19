76e3eba568bd128ebc83f814591c61302e9d91e3
Daniel Tebbutt <moeruze.burning@gmail.com>
2018-01-20 01:46:56 +1300

    Files

--- Unity/Assets/GamepadController.cs
+++ Unity/Assets/GamepadController.cs
@@ -11,9 +11,13 @@ public class GamepadController : MonoBehaviour {
 
     public Sprite[] directions;
 
-    Vector2 actualPosition;
+    Vector3 actualPosition;
     Vector3 direction;
     Vector3 targetDirection;
+
+    public Transform shadow;
+
+    float upVel;
 
 	// Use this for initialization
 	void Start () {
@@ -49,17 +53,43 @@ public class GamepadController : MonoBehaviour {
             GetComponent<SpriteRenderer>().sprite = directions[intDir];
         }
 
+        if (upVel <= 0 && actualPosition.z == 0 && Input.GetButton("Fire1"))
+        {
+            upVel = 2.5f;
+        }
+        if (actualPosition.z > 0 || upVel > 0)
+        {
+            actualPosition.z += upVel;
+            upVel -= 0.1f;
+        }
+        else
+        {
+            upVel = 0;
+            actualPosition.z = 0;
+        }
+
         var proportionSpeed = Mathf.Max(0.1f, 120 - Mathf.Abs(Vector3.Angle(direction, targetDirection))) / 120f;
-        Debug.Log(Vector2.Angle(direction, targetDirection));
+        Debug.Log(Vector3.Angle(direction, targetDirection));
 
-        actualPosition += input * maxSpeed * proportionSpeed;
+        actualPosition += (Vector3)(input * maxSpeed * proportionSpeed);
 
-        Debug.DrawRay(actualPosition, direction * 20);
+        var screenPosition = WorldToScreen(actualPosition);
+
+        Debug.DrawRay(screenPosition, direction * 20);
+
+        
 
         if (snapped)
-            transform.localPosition = new Vector2(Mathf.RoundToInt(actualPosition.x), Mathf.RoundToInt(actualPosition.y));
+            transform.localPosition = new Vector2(Mathf.RoundToInt(screenPosition.x), Mathf.RoundToInt(screenPosition.y));
         else
-            transform.localPosition = actualPosition;
+            transform.localPosition = screenPosition;
 
+        shadow.localPosition = WorldToScreen(new Vector3(actualPosition.x, actualPosition.y, 0)) + Vector2.down * 14;
+        shadow.localScale = new Vector3(0.4f - actualPosition.z / 100, 0.4f - actualPosition.z / 100, 1);
+    }
+
+    public Vector2 WorldToScreen(Vector3 input)
+    {
+        return new Vector2(input.x, input.y / Mathf.Sqrt(2) + input.z / Mathf.Sqrt(2));
     }
 }
