c64925595861a66d465f541473f5d9790ed6ac5d
Daniel Tebbutt <moeruze.burning@gmail.com>
2018-01-20 14:10:19 +1300

    Pixeltools

---
+++ PixelTools/Assets/MenuOptions.cs
@@ -0,0 +1,26 @@
+﻿using SFB;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MenuOptions : MonoBehaviour {
+    
+    public void PixelDepth()
+    {
+        string[] files = StandaloneFileBrowser.OpenFilePanel("Select spritesheet", ".", new[] { new ExtensionFilter("Spritesheet PNG", "png") }, false);
+
+        if (files.Length == 0)
+            return;
+
+        var bytes = File.ReadAllBytes(files[0]);
+
+        Texture2D tex = new Texture2D(1, 1);
+        tex.LoadImage(bytes);
+
+        var sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), Vector2.zero);
+
+        FindObjectOfType<SpriteRenderer>().sprite = sprite;
+    }
+
+}
